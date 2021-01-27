cd2838dd45a52fe06367985347899b162bf5201b
Alexandr Savca <alexandr.savca89@gmail.com>
2021-01-27 11:17:22 +0200

    config.mk: adjust for zeppelin

--- config.mk
+++ config.mk
@@ -12,5 +12,5 @@ RANLIB = ranlib
 # for NetBSD add -D_NETBSD_SOURCE
 # -lrt might be needed on some systems
 CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_XOPEN_SOURCE=700 -D_FILE_OFFSET_BITS=64
-CFLAGS   = -std=c99 -Wall -pedantic
-LDFLAGS  = -s
+CFLAGS   += -std=c99 -Wall -pedantic
+#LDFLAGS  = -s
